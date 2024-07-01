4b8151a196298ca7200c77d3311989a32231e811
Mohammad Mahdi Karami <mo.mahdi.ka@gmail.com>
2024-07-01 16:35:27 +0330

    add sc to head (styled components)

---
+++ src/colors/color-palette.js
@@ -0,0 +1,13 @@
+const colorPalette = {
+  mainBG: "#202020",
+  mainText: "#e9e9e9",
+  mainColor: "#1ab30f",
+  mainColorDark: "#147a0d",
+  secondaryColor: "#ccc",
+  secondaryColorDark: "#2c2c2c",
+  danger: "#ff0000",
+};
+
+// const { mainBG , mainText, mainColor, mainColorDark, secondaryColor, secondaryColorDark, danger } = colorPalette;
+
+export default colorPalette;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,30 @@
 //? react imports
 import { useEffect, useState } from "react";
+//? styled components
+import styled from "styled-components";
+import colorPalette from "../colors/color-palette";
+const { mainColor, mainColorDark, secondaryColor, secondaryColorDark, danger } = colorPalette;
+const Head = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: ${secondaryColorDark};
+  padding: 1rem 2rem;
+  border-radius: 5px;
+  margin: 2rem 0;
+`;
+const Input = styled.input`
+  border-radius: 5px;
+  border: none;
+  padding: 0.6rem 1.2rem;
+  font-size: 1em;
+  outline: none;
+  background: ${secondaryColor};
+  &::placeholder {
+    color: ${secondaryColorDark};
+  }
+`;
 
 function Header({ initialBooks, setBooks }) {
   const [search, setSearch] = useState("");
@@ -9,12 +34,12 @@ function Header({ initialBooks, setBooks }) {
     setBooks(newBooks);
   }, [search]);
   return (
-    <header>
-      <div className="header">
-        <input value={search} type="text" placeholder="Search" onChange={({ target }) => setSearch(target.value)} />
-        <a href="https://github.com/mahdl-karami">icon</a>
-      </div>
-    </header>
+    <Head>
+      <Input value={search} type="text" placeholder="Search" onChange={({ target }) => setSearch(target.value)} />
+      <a target="_Blank" href="https://github.com/mahdl-karami">
+        icon
+      </a>
+    </Head>
   );
 }
 
